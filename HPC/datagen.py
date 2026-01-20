dcf01fb89acba0e2593ee25cff9ec319da27ea66
spar5an <120607143+spar5an@users.noreply.github.com>
2026-01-20 01:30:07 +0000

    working on adding 2 detectors for lstm, something is wrong with it

--- HPC/datagen.py
+++ HPC/datagen.py
@@ -6,6 +6,6 @@ config = {
     "spin1z": lambda size: np.random.uniform(-0.5, 0.5, size=size)
 }
 
-output = pycbc_data_generator(config, 40000, show_progress=False, num_workers=16, add_noise=False)
+output = pycbc_data_generator(config, 10000, show_progress=True, num_workers=16, add_noise=True, batch_size=1, time_resolution=1/256)
 
 save_dataloaders(output, "data_no_noise.pt")
